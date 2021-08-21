f369df06ea1ec5ef39e1605f8f1bd4f2453ca32d
Gnoyoyo <genoyoyo@gmail.com>
2021-08-21 12:46:24 +0700

    Set up Database adapter

---
+++ src/adapter/database.ts
@@ -0,0 +1,28 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class Databases {
+	async createList(request:any) {
+
+    }
+
+	async getList(request:any) {
+
+    }
+
+	async CreateTask(request:any) {
+
+    }
+
+    async getTasks(request:any) {
+
+    }
+
+    async moveTask(request:any) {
+
+    }
+}   
+
+
+export default Databases;
